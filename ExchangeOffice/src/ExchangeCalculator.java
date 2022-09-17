e4ed484969bd962884bb47761d1122ec9bc5a1af
Ghonim Ayoub <ghonim.ayoub1@gmail.com>
2022-09-17 21:22:21 +0100

    j'ai ajouté des setters et getters

--- ExchangeOffice/src/ExchangeCalculator.java
+++ ExchangeOffice/src/ExchangeCalculator.java
@@ -1,10 +1,34 @@
 public class ExchangeCalculator {
-    int currencyInitial;
-    float exchangeRate;
-    float currencyChangeable;
+    private int currencyInitial;
+    private float exchangeRate;
+    private float currencyChangeable;
 
     public float getCurrencyChangeable(int currencyInitial, float exchangeRate) {
         currencyChangeable = currencyInitial * exchangeRate;
         return currencyChangeable;
+    }
+
+    public int getCurrencyInitial() {
+        return currencyInitial;
+    }
+
+    public void setCurrencyInitial(int currencyInitial) {
+        this.currencyInitial = currencyInitial;
+    }
+
+    public float getExchangeRate() {
+        return exchangeRate;
+    }
+
+    public void setExchangeRate(float exchangeRate) {
+        this.exchangeRate = exchangeRate;
+    }
+
+    public float getCurrencyChangeable() {
+        return currencyChangeable;
+    }
+
+    public void setCurrencyChangeable(float currencyChangeable) {
+        this.currencyChangeable = currencyChangeable;
     }
 }
